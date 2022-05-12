f774e72fbe923745d3625e9ea9a163ffe344ebde
Joceline Marquez <andrea.marquezvivaldo@gmail.com>
2022-05-12 15:43:00 -0500

    Creating components with functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+function Header(props){
+
+  return(
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+      </header>
+  );
+}
+
+function Main(props) {
+  return(
+    <section>
+      <p>We serve the most {props.adjective} food </p>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+          <li>{dish}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Cheesy Pizza",
+  "Salmon with pasta"
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Joss" />
+      <Main adjective="delicious" dishes={dishes} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
